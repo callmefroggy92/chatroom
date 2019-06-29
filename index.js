252d0ab5ed5f8020357b490c8373a7fbb5f2a157
unknown <loganholland1992@gmail.com>
2019-06-29 17:45:00 -0500

    Week 10 Chatroom

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/chat', function (req, res){
-	res.send('Key: ');
+	res.send('Key: ' + req.body.key);
 	}
   )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

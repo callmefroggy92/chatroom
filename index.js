5af70aa4dd4f58367d19874d58bbd82b32fd1b2b
unknown <loganholland1992@gmail.com>
2019-06-29 17:55:17 -0500

    Week 10 Chatroom

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express()
 app.use(express.json())       
 app.use(express.urlencoded()) 
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
